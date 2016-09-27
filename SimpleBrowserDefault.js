24c78e774808eddb28bb6e7b6a3deea1239ce7d2
SarahTsaki <tsakichick@gmail.com>
2016-09-27 13:11:29 -0400

    Create SimpleBrowserDefault.js

---
+++ SimpleBrowserDefault.js
@@ -0,0 +1,11 @@
+    Polymer({
+      themecolor: '#555555',
+      homeaddress: 'https://simplebrowsernewtab.weebly.com/',
+      page1address: 'https://simplebrowsernewtab.weebly.com/',
+      page2address: 'https://simplebrowsernewtab.weebly.com/',
+      page3address: 'https://simplebrowsernewtab.weebly.com/',
+      page4address: 'https://simplebrowsernewtab.weebly.com/',
+      page5address: 'https://simplebrowsernewtab.weebly.com/',
+      active: 'fullscreen',
+      test: true
+    });
